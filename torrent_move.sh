c6acc2946dbe5bbca4e058ddb8488ef70c2e4c53
carlos coelho <carloscoelho_@msn.com>
2019-04-11 14:30:25 -0300

    Create torrent_move.sh

---
+++ torrent_move.sh
@@ -0,0 +1,7 @@
+cd /home/zombie/apps/torrent/torren-down
+sudo mv *.exe *.rar *.zip /home/zombie/apps/download
+sudo mv *.mp4 *.avi  /home/zombie/media/filmes
+sudo mv *.mp3 *.flac *.wav  /home/zombie/media/music
+sudo mv *.rar   /home/zombie/media/games
+sudo mv *.bin *.cue *.iso /home/zombie/app/downloads
+cd
