34eacca14ff39ce60202a9929118412a42aa17fb
carlos coelho <carloscoelho_@msn.com>
2019-04-17 11:29:23 -0300

    Update torrent_move.sh

--- torrent_move.sh
+++ torrent_move.sh
@@ -1,7 +1,16 @@
-cd /home/zombie/apps/torrent/torren-down
-sudo mv *.exe *.rar *.zip /home/zombie/apps/download
-sudo mv *.mp4 *.avi  /home/zombie/media/filmes
-sudo mv *.mp3 *.flac *.wav  /home/zombie/media/music
-sudo mv *.rar   /home/zombie/media/games
-sudo mv *.bin *.cue *.iso /home/zombie/app/downloads
-cd
+#!/bin/bash
+# diretorio para fazer a busca
+dir="/home/zombie/apps/torrent/torrent-down"
+# diretorio para enviar os arquivos
+iso="/home/zombie/games/instalacao"
+rar="/home/zombie/apps/download"
+
+# a script to recursively find and copy files to a desired location
+find $dir -type f -iname '*.iso' -print0 |
+while IFS= read -r -d '' f; 
+do mv -- "$f" $iso ;
+
+find $dir -type f -iname '*.rar *.exe *.zip' -print0 |
+while IFS= read -r -d '' f; 
+do mv -- "$f" $rar ;
+done
